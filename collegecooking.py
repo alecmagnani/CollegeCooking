4cf454d28b3f32b579c74373fa2de204381fa25b
alecmagnani <alecmagnani@gmail.com>
2016-07-09 14:48:34 -0400

    cleaned code and updated search to include all results instead of just the first page. ~1000 results for a basic search vs 10 results previously

--- collegecooking.py
+++ collegecooking.py
@@ -4,73 +4,35 @@ import sys
 import urllib2
 import webbrowser
 
-def generateURL(search_query, search_ingredients = [], *args):
+def generateURL(page, search_query, search_ingredients = [], *args):
     # basic search url
     url = "http://www.recipepuppy.com/api?"
-    
     # add search ingredients
     if len(search_ingredients) > 0:
         url = url + "i="
-
         for x in range(0, len(search_ingredients)):
             url = url + search_ingredients[x]
-
             if x < len(search_ingredients) - 1:
                 url = url + ","
-
     # add search query
     if search_query != None:
         url = url + "&q=" + search_query
 
+    if page != 0:
+        url = url + "&p=" + str(page)
     return url
 
-search_ingredients = []
-search_query = None
-url = None
+def getRecipes(page, search_query, search_ingredients = [], recipes = [], *args):
+        url = generateURL(page, search_query, search_ingredients)
 
-while True:
-    print ""
-    print "Welcome to the College Cookbook"
-    print ""
-    print "Press 1 to add specific ingredients"
-    print "Press 2 to add a specific search term"
-    print "Press 3 to grab a recipe"
-    print "Press 4 to reset ingredients and search term"
-    print "Press 5 to quit"
-
-    user_choice = raw_input("> ")
-    print ""
-
-    # Add search ingredients
-    if user_choice == "1":
-        print "Enter ingredients you would like to use"
-        print "To stop adding ingredients, leave the prompt empty and press ENTER"
-        while True:
-            ingr = raw_input("> ")
-            if ingr == "":
-                break;
-            else:
-                search_ingredients.append(ingr)
-
-    # Add search query
-    elif user_choice == "2":
-        print "Enter a search query"
-        query = raw_input("> ")
-        search_query = query
-
-    # Search for recipes
-    elif user_choice == "3":
-        url = generateURL(search_query, search_ingredients)
         response = urllib2.urlopen(url)
-        html = response.read()
-        recipes = []
+        json = response.read()
 
         # get rid of API header
-        raw_recipes = html.split("[")[1]
+        raw_recipes = json.split("[")[1]
         raw_recipes = raw_recipes.translate(None, "]")
 
         # split into individual recipes
-        success = False
         individuals = raw_recipes.split("},{")
         for recipe in individuals:
             recipe = recipe.translate(None, "{")
@@ -91,37 +53,89 @@ while True:
 
                 recipe = Recipe(title[1], link[1], ingredients[1])
                 recipes.append(recipe)
-                success = True
+            except:
+                return recipes
 
-            except IndexError:
-                print ""
-                print "Sorry, no results found."
-                print "Try different ingredients, or a different search term"
-                print "Resetting search"
-                Recipe.recipeCount = 0
-                search_ingredients = []
-                search_query = None
+        getRecipes(page + 1, search_query, search_ingredients, recipes) 
+        return recipes
 
-        if success == True:
+def getRandom(recipes):
+    num = randint(0, len(recipes) - 1)
+    return recipes[num]
 
-            number = randint(0, Recipe.recipeCount - 1)
-            print""
-            recipes[number].display()
+if __name__ == "__main__":
+
+    allRecipes = []
+    recipes = []
+    search_ingredients = []
+    search_query = None
+    url = None
+
+    while True:
+        print ""
+        print "Welcome to the College Cookbook"
+        print ""
+        print "Press 1 to add specific ingredients"
+        print "Press 2 to add a specific search term"
+        print "Press 3 to grab a recipe"
+        print "Press 4 to reset ingredients and search term"
+        print "Press 5 to quit"
+
+        user_choice = raw_input("> ")
+        print ""
+
+        # Add search ingredients
+        if user_choice == "1":
+            print "Enter ingredients you would like to use"
+            print "To stop adding ingredients, leave the prompt empty and press ENTER"
+            while True:
+                ingr = raw_input("> ")
+                if ingr == "":
+                    break;
+                else:
+                    search_ingredients.append(ingr)
+
+        # Add search query
+        elif user_choice == "2":
+            print "Enter a search query"
+            query = raw_input("> ")
+            search_query = query
+
+        # Search for recipes
+        elif user_choice == "3":
+            if len(allRecipes) == 0:
+                print("Loading recipes...")
+                allRecipes = getRecipes(0, search_query, search_ingredients, recipes)
+                recipe = getRandom(allRecipes)
+            else:
+                recipe = getRandom(allRecipes)
+
             print ""
-            print "Look interesting? Press ENTER to open the recipe, press anything else to keep moving"
-            response = raw_input("> ")
-            if not response: #input was an ENTER
-                print "Opening recipe in browser"
-                webbrowser.open_new_tab(recipes[number].link)
+            print "Picked from " + str(Recipe.recipeCount) + " recipes:"
+            recipe.display()
+            while True:
+                print ""
+                print "Does this look interesting?"
+                print "Press 1 to open"
+                print "Press 2 to grab a new recipe"
+                print "Press any other key to go to the menu"
+                print ""
+                choice = raw_input("> ")
+                if choice == "1":
+                    webbrowser.open_new_tab(recipe.link)
+                    break
+                elif choice == "2":
+                    recipe = getRandom(allRecipes)
+                    recipe.display()
+                else:
+                    break
 
-            # Reset the recipe counter
+        elif user_choice == "4":
+            allRecipes = []
+            recipes = []
+            search_ingredients = []
+            search_query = None
             Recipe.recipeCount = 0
 
-    elif user_choice == "4":
-        # Reset search terms and recipe counter
-        search_ingredients = []
-        search_query = None
-        Recipe.recipeCount = 0
-
-    elif user_choice == "5":
-        sys.exit()
+        elif user_choice == "5":
+            sys.exit()
