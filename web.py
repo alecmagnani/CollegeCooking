13f7ac4478772d2794ad2ee46d2d129f0b4f6f24
alecmagnani <alecmagnani@gmail.com>
2016-09-01 23:13:55 -0400

    website progress

--- web.py
+++ web.py
@@ -2,6 +2,7 @@ from flask import (
         Flask,
         request,
         render_template,
+        redirect,
 )
 
 import whatsfordinner
@@ -10,7 +11,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home(recipe = None, usr_ingredients = None):
-    url = whatsfordinner.getIngredientSearchURL(None)
+    usr_ingredients = whatsfordinner.importIngredients("ingredients.txt")
+    url = whatsfordinner.getIngredientSearchURL(None, usr_ingredients)
+    print(url)
     recipes = whatsfordinner.ingredientSearch(url)
     recipe = whatsfordinner.getRandomRecipe(recipes)
 
@@ -24,15 +27,14 @@ def home_add_ingredient(recipe=None, usr_ingredients=None):
     ingredient = request.form['Add Ingredient']
     
     usr_ingredients = whatsfordinner.importIngredients("ingredients.txt")
-    usr_ingredients.append(ingredient)
+    if ingredient not in usr_ingredients:
+        usr_ingredients.append(ingredient)
+    else:
+        usr_ingredients.remove(ingredient)
     whatsfordinner.deleteIngredients("ingredients.txt")
     whatsfordinner.writeIngredients("ingredients.txt", usr_ingredients)
 
-    url = whatsfordinner.getIngredientSearchURL(None)
-    recipes = whatsfordinner.ingredientSearch(url)
-    recipe = whatsfordinner.getRandomRecipe(recipes)
-
-    return render_template('webpage.html', recipe=recipe, usr_ingredients=usr_ingredients)
+    return redirect('/')
 
 
 if __name__ == '__main__':
